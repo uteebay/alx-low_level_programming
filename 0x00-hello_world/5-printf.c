774d6f0b96b6894dd9bd2befb4752a245ee00f53
utibe akanimoh <akanimohutibe@gmail.com>
2023-02-20 16:52:33 -0800

    "printf"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A Programme that prints a line using the printf function
+ * main - Programme that prints a line using the printf function
  * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
